d0ad3cfe124d6805290f9987546e9bf261ab4e36
Nurul Haque <safhum87@hotmail.com>
2018-12-11 08:54:04 -0600

    TAsk added

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function add(a,b){
-    document.getElementById('addmath').innerHTML=a+b;
+function suggestion() {
+    var eventType = prompt("What's your event?", "");
+    var tempFahr = prompt("What's the temperature in Farenheit? ", "");
+    var result = "";
+
+    if (eventType == "casual") {
+        result = "something comfy";
+
+    } else if (eventType == "semi-formal") {
+        result = "a polo";
+    } else if (eventType == "formal") {
+        result = "a suit";
+    } 
+    
+
+    if (tempFahr < 54) {
+        result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear " + result + " and a coat.";
+    }else if(tempFahr >54 && tempFahr <70){
+    	result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear " + result + " and a jacket ";
+
+    }
+    else if (tempFahr >70){
+    	result = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear " + result + " and no jacket";
+    }
+   
+
+
+    console.log(result);
+    
 }
-function sub(a,b){
-    document.getElementById('addmath').innerHTML=a-b;
-}
+suggestion();
